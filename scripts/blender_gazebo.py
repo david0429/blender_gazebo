3ef36946053552b81d2b24326eba5b9283face0a
Dave Niewinski <david_0429@hotmail.com>
2019-09-30 14:10:26 -0400

    Added base launch and urdf files

--- scripts/blender_gazebo.py
+++ scripts/blender_gazebo.py
@@ -1,7 +1,8 @@
 import bpy
 import os
 import subprocess
-
+import xml.etree.ElementTree as ET
+ 
 collision_suffix = "_collision"
 prefix = ""
 
@@ -9,11 +10,12 @@ def checkDir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def getPackagePaths(package_name):
+def getPackagePaths(package_name, make_dirs=True):
     out = subprocess.Popen(['rosrun', 'blender_gazebo', 'rospack.py', package_name], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout,stderr = out.communicate()
     out = stdout.decode().strip()
-    if "ERROR" in str(out):
+    
+    if "ERROR" in str(out).upper() or "rosbash" in str(out):
         return [None, None, None, None]
     else:
         r_values = out.split(",")
@@ -21,40 +23,76 @@ def getPackagePaths(package_name):
             checkDir(dir)
         return r_values
 
-def writeURDF(name, directory, visual, collision):
-    pass
-
+def writeList(location, list):
+    out_file = open(location, 'w')
+    for line in list:
+        out_file.write(line)
+    out_file.close()
+    
+def writeURDF(name, package_name, visual, collision, urdf_dir, blender_gazebo_root_dir):
+    body_urdf_xml = blender_gazebo_root_dir + "/data/body.urdf.xacro"
+    out_data = []
+    
+    in_file = open(body_urdf_xml, 'r')
+    for line in in_file:
+        out_data.append(line.replace("$NAME$", name).replace("$PACKAGE$", package_name))
+    in_file.close()
+    
+    writeList(urdf_dir + name + ".urdf.xacro", out_data)
+    
+def writeLaunch(name, package_name, launch_dir, blender_gazebo_root_dir):
+    body_launch = blender_gazebo_root_dir + "/data/body.launch"
+    out_data = []
+    
+    in_file = open(body_launch, 'r')
+    for line in in_file:
+        out_data.append(line.replace("$NAME$", name).replace("$PACKAGE$", package_name))
+    in_file.close()
+    
+    writeList(launch_dir + "/spawn_" + name + ".launch", out_data)
+    
 def exportSTL(ob, name, dir):
     bpy.ops.object.select_all(action='DESELECT')
     ob.select_set(True)
-    bpy.ops.export_mesh.stl(filepath= dir + name + ".stl", use_selection=True, use_mesh_modifiers=True)
+    bpy.ops.export_mesh.stl(filepath= dir + name, use_selection=True, use_mesh_modifiers=True)
 
 def exportDAE(ob, name, dir):
     bpy.ops.object.select_all(action='DESELECT')
     ob.select_set(True)
-    bpy.ops.wm.collada_export(filepath= dir + name + ".dae", apply_modifiers=True, selected=True)
+    bpy.ops.wm.collada_export(filepath= dir + name, apply_modifiers=True, selected=True)
 
 def main():
     global prefix
     prefix = bpy.path.basename(bpy.context.blend_data.filepath)
     prefix = prefix[:prefix.rindex(".")]
     
-    root_dir, mesh_dir, urdf_dir, launch_dir = getPackagePaths("sw_gazebo_test")
-    if root_dir:
-        for ob in bpy.data.objects:
-            if ob.type == 'MESH':
-                if collision_suffix not in ob.name:
-                    print("Working on " + ob.name)
-                    visual_name = prefix + "_" + ob.name
-                    exportDAE(ob, visual_name, mesh_dir)
+    blender_gazebo_root_dir, mesh_dir, urdf_dir, launch_dir = getPackagePaths("blender_gazebo")
+    if blender_gazebo_root_dir:
+        package_name = "sw_gazebo_test"
+        root_dir, mesh_dir, urdf_dir, launch_dir = getPackagePaths(package_name)
+        
+        if root_dir:
+            for ob in bpy.data.objects:
+                if ob.type == 'MESH':
+                    if collision_suffix not in ob.name:
+                        print("Working on " + ob.name)
+                        body_name = prefix + "_" + ob.name
+                        visual_name = body_name + ".dae"
+                        exportDAE(ob, visual_name, mesh_dir)
 
-                    collision_name = ob.name + collision_suffix
-                    try:
-                        collision_file = bpy.data.objects[collision_name]
-                        print("Found custom collision mesh")
-                        collision_name = visual_name + collision_suffix
-                        exportSTL(collision_file, collision_name, mesh_dir)
-                    except:
-                        collision_name = visual_name
-
+                        collision_name = ob.name + collision_suffix
+                        try:
+                            collision_file = bpy.data.objects[collision_name]
+                            print("Found custom collision mesh")
+                            collision_name = body_name + collision_suffix + ".stl"
+                            exportSTL(collision_file, collision_name, mesh_dir)
+                        except:
+                            collision_name = visual_name
+                            
+                        writeURDF(body_name, package_name, visual_name, collision_name, urdf_dir, blender_gazebo_root_dir)
+                        writeLaunch(body_name, package_name, launch_dir, blender_gazebo_root_dir)
+        else:
+            print("Error finding desired package.  Is your workspace sourced properly?")
+    else:
+        print("Error finding blender_gazebo.  Is ROS or your workspace sourced properly?")
 main()
